49cec46f9344757f020b74b48301c743cac893ff
Puntawat Subhamani <puntawatsub@gmail.com>
2025-12-06 00:32:03 +0200

    integrate frontend with express server

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const jobRouter = require('./routes/jobRouter')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const jobRouter = require("./routes/jobRouter");
 const {
   unknownEndpoint,
   errorHandler,
-} = require('./middleware/customMiddleware')
-const connectDB = require('./config/db')
-const cors = require('cors')
+} = require("./middleware/customMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.static("view"));
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
 
-connectDB()
+connectDB();
 
 // Use the jobRouter for all "/jobs" routes
-app.use('/api/jobs', jobRouter)
+app.use("/api/jobs", jobRouter);
 
-app.use('/api', unknownEndpoint)
-app.use(errorHandler)
+app.use("/api", unknownEndpoint);
+app.use(errorHandler);
 
-module.exports = app
+app.use((req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
+});
+
+module.exports = app;
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server running on port ${process.env.PORT}`)
